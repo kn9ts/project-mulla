e9ac75db568df0d47c4f21c865f168f706200b35
Eugene Mutai <eugenemutai@gmail.com>
2016-03-04 10:43:17 +0300

    Cleared master for a reset in development

--- requestStatus.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type TransactionStatusRequest struct {
-	// Optional, though if not defined then MERCHANT_TRANSCTION_ID is required to be defined.
-	TRX_ID                  string
-	MERCHANT_TRANSACTION_ID string
-}
