cf0662a6a1e26b0444c0b75215d371cb31b12a16
Eugene Mutai <eugenemutai@gmail.com>
2016-03-13 23:27:50 +0300

    Created bootstrap script for the SOAP to REST configs

--- server/server/boot/root.js
+++ server/server/boot/root.js
@@ -1,6 +1,6 @@
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+module.exports = function(app) {
+    // Install a `/` route that returns app status
+    var router = app.loopback.Router();
+    router.get('/', app.loopback.status());
+    app.use(router);
 };
---
+++ server/server/boot/soap-bootstrap.js
@@ -0,0 +1,4 @@
+// This script bootstraps the SOAP to REST configurations
+// Much more can be done, for now still figuring how
+module.exports = function(app, callback) {};
+
