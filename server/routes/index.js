f4ba0df5a6dfb5e8e52cc5df2cc3edae7e96fc63
Eugene Mutai <eugenemutai@gmail.com>
2016-04-26 00:28:05 +0300

    Added process checkout route for testing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,20 @@
-export default function(router) {
-  'use strict';
+import uuid from 'node-uuid';
+import cRequest from '../controllers/checkout-request.js';
 
+export default function(router) {
   /* Load up the homepage */
-  router.get('/*', function(req, res) {
+  router.get('/', function(req, res) {
     // res.sendFile('index.html', {root: './public/'});
     res.render('index', { title: 'Express' });
+  });
+
+  router.get('/request/checkout', function(req, res) {
+    cRequest.send(cRequest.constructSOAPBody({
+      referenceID: uuid.v4(),
+      amountInDoubleFloat: '10.00',
+      clientPhoneNumber: '254723001575'
+    }));
+    res.json({ 'status': true });
   });
 
   return router;
