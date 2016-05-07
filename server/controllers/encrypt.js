7c6354d5494fa5b356e61190bf0a77c8484ed90d
Eugene Mutai <eugenemutai@gmail.com>
2016-05-07 19:30:23 +0300

    Refactored the password encryption algorithmn

--- server/controllers/encrypt.js
+++ server/controllers/encrypt.js
@@ -4,7 +4,9 @@ export default class EncryptedPassword {
   constructor(timeStamp) {
     let concatenatedString = [process.env.PAYBILL_NUMBER, process.env.PASSKEY, timeStamp].join('');
     let hash = crypto.createHash('sha256');
-    this.hashedPassword = hash.update(concatenatedString).digest('base64');
-    this.hashedPassword = this.hashedPassword.toUpperCase();
+    this.hashedPassword = hash.update(concatenatedString).digest('hex'); // or 'binary'
+    this.hashedPassword = new Buffer(this.hashedPassword).toString('base64');
+    // this.hashedPassword = this.hashedPassword.toUpperCase();
+    // console.log('hashedPassword ==> ', this.hashedPassword);
   }
 }
