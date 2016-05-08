e87711cd20f520031311da4bbc21cde737fadb74
Eugene Mutai <eugenemutai@gmail.com>
2016-05-08 19:31:26 +0300

    Added status code interpretation method

--- server/controllers/parse-response.js
+++ server/controllers/parse-response.js
@@ -1,8 +1,10 @@
 import cheerio from 'cheerio';
+import _ from 'lodash';
+import statusCodes from '../config/status-codes';
+
 
 export default class ParseResponse {
   constructor(soapResponse) {
-    // console.log(soapResponse);
     // Remove the XML header tag
     soapResponse = soapResponse.replace(/\<\?[\w\s\=\.\-\'\"]+\?\>/gmi, '');
 
@@ -26,23 +28,29 @@ export default class ParseResponse {
   }
 
   toJSON() {
-    // console.log(this.response);
-    let soapTree = cheerio.load(this.response, {
-      xmlMode: true
-    });
-
-    let JSON = {};
-    soapTree('processcheckoutresponse').children().each((i, el) => {
+    this.json = {};
+    let $ = cheerio.load(this.response, { xmlMode: true });
+    $('processcheckoutresponse').children().each((i, el) => {
       if (el.children.length > 1) {
         console.log('Has more than one child.');
       }
 
       if (el.children.length === 1) {
         // console.log(el.name, el.children[0].data);
-        JSON[el.name] = el.children[0].data;
+        this.json[el.name] = el.children[0].data.replace(/\s{2,}/gi, ' ').replace(/\n/gi, '').trim();
       }
     });
 
-    return JSON;
+    // Unserialise the ENC_PARAMS value
+    this.json.enc_params = JSON.parse(this.json.enc_params)
+
+    // Get the equivalent HTTP CODE to respond with
+    this.json = _.assignIn(this.extractCode(), this.json);
+    delete this.json.return_code;
+    return this.json;
+  }
+
+  extractCode() {
+    return _.find(statusCodes, (o) => o.returnCode == this.json.return_code);
   }
 }
