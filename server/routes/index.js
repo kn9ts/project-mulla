4b91c3c29d268a2891ca12fd6e62a3e30428dc8c
Eugene Mutai <eugenemutai@gmail.com>
2016-05-16 23:38:26 +0300

    Refactored error handling of the responses; Moved to it's own class

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,56 +1,48 @@
 import uuid from 'node-uuid';
+import ResponseError from '../controllers/errorhandler';
 import PaymentRequest from '../controllers/payment-request';
 import ConfirmPayment from '../controllers/payment-confirm';
 import PaymentStatus from '../controllers/payment-status';
 
 
 export default function(router) {
-  /* Load up the homepage */
+  /* Check the status of the API system */
   router.get('/', function(req, res) {
     return res.json({ 'status': 200 });
   });
 
   router.get('/payment/request', function(req, res) {
-    let request = PaymentRequest.send(PaymentRequest.constructSOAPBody({
+    let request = PaymentRequest.send(PaymentRequest.construct({
       referenceID: uuid.v4(), // product, service or order ID
       merchantTransactionID: uuid.v1(), // time-based
-      amountInDoubleFloat: '20.00',
+      amountInDoubleFloat: '10.00',
       clientPhoneNumber: '254723001575',
       extraMerchantPayload: JSON.stringify({ 'extra': 'info', 'as': 'object' })
     }));
 
     // process request response
-    request.then((response) => res.json(response)).catch((_error) => {
-      let err = new Error('description' in _error ? _error.description : _error);
-      err.status = 'httpCode' in _error ? _error.httpCode : 500;
-      res.status(err.status).json({ response: _error });
-    });
+    request.then((response) => res.json(response))
+      .catch((_error) => ResponseError.handler(_error, res));
   });
 
   router.get('/payment/confirm/:id', function(req, res) {
-    let confirm = ConfirmPayment.send(ConfirmPayment.constructSOAPBody({
-      transactionID: req.params.id
+    let confirm = ConfirmPayment.send(ConfirmPayment.construct({
+      transactionID: req.params.id // eg. '99d0b1c0237b70f3dc63f36232b9984c'
     }));
 
     // process ConfirmPayment response
-    confirm.then((response) => res.json(response)).catch((_error) => {
-      let err = new Error('description' in _error ? _error.description : _error);
-      err.status = 'httpCode' in _error ? _error.httpCode : 500;
-      res.status(err.status).json({ response: _error });
-    });
+    confirm.then((response) => res.json(response))
+      .catch((_error) => ResponseError.handler(_error, res));
   });
 
   router.get('/payment/status/:id', function(req, res) {
-    let status = PaymentStatus.send(PaymentStatus.constructSOAPBody({
+    let status = PaymentStatus.send(PaymentStatus.construct({
       transactionID: req.params.id // eg. '99d0b1c0237b70f3dc63f36232b9984c'
     }));
 
     // process PaymentStatus response
-    status.then((response) => res.json(response)).catch((_error) => {
-      let err = new Error('description' in _error ? _error.description : _error);
-      err.status = 'httpCode' in _error ? _error.httpCode : 500;
-      res.status(err.status).json({ response: _error });
-    });
+    status.then((response) => res.json(response))
+      .catch((_error) => ResponseError.handler(_error, res));
   });
 
   return router;
