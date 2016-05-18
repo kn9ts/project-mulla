b98f6e589f3eb16550dfe7ef0f5ed2af7bee9f03
Eugene Mutai <eugenemutai@gmail.com>
2016-05-19 02:17:20 +0300

    Removed prefixed underscore from error variable

--- server/controllers/response-error.js
+++ server/controllers/response-error.js
@@ -1,5 +1,5 @@
-export default function ResponseError(_error, res) {
-  let err = new Error('description' in _error ? _error.description : _error);
-  err.status = 'http_code' in _error ? _error.http_code : 500;
-  return res.status(err.status).json({ response: _error });
+export default function ResponseError(error, res) {
+  let err = new Error('description' in error ? error.description : error);
+  err.status = 'http_code' in error ? error.http_code : 500;
+  return res.status(err.status).json({ response: error });
 }
