a940a1c02cb1eee3227b996b6b505f129dc8984c
Eugene Mutai <eugenemutai@gmail.com>
2016-05-24 22:34:43 +0300

    Refactored responseError function syntax style

--- server/errors/ResponseError.js
+++ server/errors/ResponseError.js
@@ -1,7 +1,9 @@
 'use strict';
 
-module.exports = function ResponseError(error, res) {
+let responseError = (error, res) => {
   const err = new Error('description' in error ? error.description : error);
   err.status = 'status_code' in error ? error.status_code : 500;
   return res.status(err.status).json({ response: error });
 };
+
+module.exports = responseError;
